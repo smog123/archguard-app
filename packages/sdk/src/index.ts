bc4a12564cbbfd1987ef57c98cecce34224da4f5
smog123 <adejumooluwasegun35@gmail.com>
2026-08-08 15:58:27 -0400

    fix(sdk): add explicit module exports and reset tsconfig paths for clean Vercel compilation

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,1 +1,8 @@
-// @archguard/sdk entrypoint
+export * from "./types";
+export * from "./footprintKeys";
+export * from "./ttlExtension";
+export * from "./registryClient";
+export * from "./extenderClient";
+export * from "./readContractValue";
+export * from "./submitTransaction";
+export {};
